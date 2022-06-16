7062d1f20d61c5208281725b6555633c9b81d4c7
zhangming <qianzm2490@gmail.com>
2022-06-16 18:21:23 +0800

    chore: 优化打包命令

--- packages/uniubi-lib-mobile-docs/scripts/build.sh
+++ packages/uniubi-lib-mobile-docs/scripts/build.sh
@@ -3,6 +3,7 @@ echo 'build start'
 cd ../uniubi-lib-mobile
 npm run build
 cd ../uniubi-lib-mobile-demo
+rm -rf yarn.lock node_modules
 npm install
 npm run build:h5:production
 cd ../uniubi-lib-mobile-docs
