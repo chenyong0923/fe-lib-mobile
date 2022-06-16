055dfab2e018684ef36d29240e92bcc627030f48
zhangming <qianzm2490@gmail.com>
2022-06-16 18:46:10 +0800

    fix: icons 样式引用目录

--- packages/uniubi-lib-mobile-demo/src/app.ts
+++ packages/uniubi-lib-mobile-demo/src/app.ts
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import '@uniubi/icons-taro/dist/style/index.css';
+import '@uniubi/icons-taro/dist/styles/index.css';
 import 'uniubi-lib-mobile/dist/style/index.less';
 
 class App extends Component {
