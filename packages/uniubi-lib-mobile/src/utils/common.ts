b56bc6828280000a95ee5a5ba54e90292d7f6abc
zhangming <qianzm2490@gmail.com>
2022-06-20 15:56:26 +0800

    feat(Image): 获取系统信息方法优化

--- packages/uniubi-lib-mobile/src/utils/common.ts
+++ packages/uniubi-lib-mobile/src/utils/common.ts
@@ -1,13 +1,35 @@
 import { ITouchEvent } from '@tarojs/components';
 import Taro from '@tarojs/taro';
 
-// 阻止事件穿透
+/**
+ * 阻止事件穿透
+ * @param {ITouchEvent} event touch 事件
+ */
 export const noop = (event: ITouchEvent) => {
   event.stopPropagation();
   event.preventDefault();
 };
 
+// 系统信息
+let systemInfo: Taro.getSystemInfoSync.Result;
+
+/**
+ * 获取系统信息
+ * @returns 系统信息
+ */
+export function getSystemInfoSync() {
+  if (systemInfo == null) {
+    systemInfo = Taro.getSystemInfoSync();
+  }
+  return systemInfo;
+}
+
+/**
+ * rpx 转 px
+ * @param {Number} size rpx 尺寸
+ * @returns px 尺寸
+ */
 export const rpxToPx = (size: number) => {
-  const { windowWidth } = Taro.getSystemInfoSync();
+  const { windowWidth } = getSystemInfoSync();
   return `${size * (windowWidth / 750)}px`;
 };
